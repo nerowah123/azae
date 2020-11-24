a9972080a077bf04cf6df0bc39f124f1bc2743f3
nerowah123 <71982364+nerowah123@users.noreply.github.com>
2020-11-24 17:59:45 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.Prefix = ".";//your prefix for bot
+exports.Token =  "Nzc4Mzc2NTAzOTQyNzA5MzEw.X7RFnA.yIz8msm7v3OTQbuGnBv3SQrHss8";//your token 
+exports.Color = `COLOR OF ALL EMBEDES`;//color   of embed
